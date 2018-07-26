af8af9464d66fa7bdc3af6e9d953493f26901900
Solon Solano <ssolano1@msn.com>
2018-07-26 00:04:18 -0400

    added question array in js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,56 @@
-var
+// create variables
+var startTimer = 30;
+
+// create questions array
+var questions = [{
+    question: "Which chart-topping songstress lent her voice in 2011's The Smurfs movie?",
+    answers: ["Taylor Swift", "Katy Perry", "Selena Gomez", "Demi Lovato"],
+    correctAnswer: "Katy Perry",
+    image:"assets/images/katyperry.gif"
+  }, {
+    question: "Which actress played Katniss Everdeen in the Hunger Games?",
+    answers: ["Chloe Moretz", "Emma Stone", "Jennifer Lawrence", "Jennifer Lopez"],
+    correctAnswer: "Jennifer Lawrence",
+    image:"assets/images/jlaw.gif"
+  }, {
+    question: "In which film did the Ewoks first appear?",
+    answers: ["Empire Strikes Back", "Star Wars", "The Force Awakens", "Return of the Jedi"],
+    correctAnswer: "Return of the Jedi",
+    image:"assets/images/ewoks.gif"
+  }, {
+    question: "Who played the fictional anti-hero Deadpool?",
+    answers: ["Ryan Gosling", "Ryan Seacrest", "Ryan Phillipe", "Ryan Reynolds"],
+    correctAnswer: "Ryan Reynolds",
+    image:"assets/images/deadpool.gif"
+  }, {
+    question: "What was the name of the monkey in the Disney movie Aladdin?",
+    answers: ["Abu", "Jafar", "Jasmine", "Lago"],
+    correctAnswer: "Abu",
+    image:"assets/images/abu.gif"
+  }, {
+    question: "What character is the movie First Blodd centered around",
+    answers: ["Rocky Balboa", "John Rambo", "John Spartan", "Judge Dredd"],
+    correctAnswer: "John Rambo",
+    image:"assets/images/rambo.gif"
+  }, {
+    question: "Who played a foul-mouth Detroit cop that goes to California to investigate his friends murder?",
+    answers: ["Samuel L Jackson", "Dwwayne Johnson", "Eddie Murphy", "Will Smith"],
+    correctAnswer: "Eddie Murphy",
+    image:"assets/images/eddie.gif"
+  }, {
+    question: "Which actress was pregnant while filming Wonder Woman?",
+    answers: ["Gal Gadot", "Robin Wright", "Connie Nielson", "Patty Jenkins"],
+    correctAnswer: "Gal Gadot",
+    image:"assets/images/gal.gif"
+  }, {
+    question: "Which 90s movie soundtrack is the best selling soundtrack of all-time?",
+    answers: ["Space Jam", "Titanic", "The Bodyguard", "Pretty Woman"],
+    correctAnswer: "The Bodyguard",
+    image:"assets/images/bodyguard.gif"
+  }, {
+    question: "Who played inspector Lee's partner in the movie Rush Hour?",
+    answers: ["Jackie Chan", "Chris Rock", "Chris Tucker", "Kevin Hart"],
+    correctAnswer: "Chris Tucker",
+    image:"assets/images/christucker.gif"
+  },];
+
